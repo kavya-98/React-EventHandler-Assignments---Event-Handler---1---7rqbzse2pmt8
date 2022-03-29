36851cec08d950a89ec9698e94bffe08db663c18
kavya-98 <76950344+kavya-98@users.noreply.github.com>
2022-03-29 21:33:53 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,14 @@
 import React from "react";
 import "../styles/App.css";
 const App = () => {
-  const handleDoubleClick = (event) => {
-    event.preventDefault();
-    console.log("I was double clicked");
+  // do not change the code inside the function clickA
+  const clickA = () => {
+    console.log("Clicked button A");
   };
-  const handleSingleClick = (event) => {
-    event.preventDefault();
-    console.log("I was not double clicked");
-  };
-
   return (
     <div id="main">
-      <button
-        onDoubleClick={handleDoubleClick}
-        onClick={handleSingleClick}
-        id="dblclick-btn"
-      >
-        Double click me
+      <button onClick={clickA} id="button-a">
+        Button A
       </button>
     </div>
   );
